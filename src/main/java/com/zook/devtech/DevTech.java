1499245cd3fb7723fd52599b8cc6df3e58f60c7f
brachy84 <brachy84@gmail.com>
2023-08-04 17:34:42 +0200

    move to rfg

--- src/main/java/com/zook/devtech/DevTech.java
+++ src/main/java/com/zook/devtech/DevTech.java
@@ -1,6 +1,7 @@
 package com.zook.devtech;
 
 import codechicken.lib.CodeChickenLib;
+import gregtech.GTInternalTags;
 import gregtech.api.GTValues;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.common.CommonProxy;
@@ -9,15 +10,20 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = DevTech.MODID,
         name = DevTech.NAME,
         version = DevTech.VERSION,
-        dependencies = "required:forge@[14.23.5.2847,);" + CodeChickenLib.MOD_VERSION_DEP + GTValues.MOD_VERSION_DEP + "after:crafttweaker")
+        dependencies = "required:forge@[14.23.5.2847,);" + CodeChickenLib.MOD_VERSION_DEP + GTInternalTags.DEP_VERSION_STRING + "after:crafttweaker")
 public class DevTech {
-    public static final String MODID = "devtech";
+
+    public static final String MODID = Tags.ID;
     public static final String NAME = "DevTech";
-    public static final String VERSION = "2.1.2";
+    public static final String VERSION = Tags.VERSION;
+
+    public static final Logger LOG = LogManager.getLogger(MODID);
 
     @SidedProxy(modId = GTValues.MODID, clientSide = "com.zook.devtech.client.ClientProxy", serverSide = "com.zook.devtech.common.CommonProxy")
     public static CommonProxy proxy;
